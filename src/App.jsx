22e45f9b0b9eedf8b74fcdf445eae860398a594b
Haya Ahmed <haya.ahm.08@gmail.com>
2026-06-21 00:11:42 +0300

    adjust structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { Navbar } from "./components/Navbar"
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
 
 export const App = () => {
-  return(
-    <main>
-      <Navbar />
-    </main>
-  )
-}
+  return (
+    <BrowserRouter>
+      <div className="">
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+
+      {/* stars */}
+      {/* <div className="relative z-0">
+          <Contact />
+        </div> */}
+    </div>
+    </BrowserRouter> 
+  );
+};
--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-  {href: "#Home", label: "Home"},
-  {href: "#About", label: "About"},
-  {href: "#Services", label: "Services"},
-  {href: "#Contact", label: "Contact"},
-]
